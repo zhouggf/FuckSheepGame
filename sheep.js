4e8f1bae8fff3cf33d4468b63b213d2ac3573d19
zhouggf <49642488+zhouggf@users.noreply.github.com>
2022-10-01 09:03:48 +0800

    Update sheep.js

--- sheep.js
+++ sheep.js
@@ -1,3 +1,4 @@
+
 /*
  * @Author: Vincent Young
  * @Date: 2022-09-15 15:21:34
@@ -11,4 +12,11 @@
 /*
 [rewrite_local]
 # > Fuck Sheep Game
-^https://cat-match-static.easygame2021\.com/maps/(.*) url script-request-header https://raw.githubusercontent.com/zhouggf/FuckSheepGame/main/sheep.js
+^https://cat-match-static.easygame2021\.com/maps/(.*) url script-request-header https://raw.githubusercontent.com/missuo/FuckSheepGame/main/sheep.js
+[mitm] 
+hostname = *.easygame2021.com
+*/
+
+path =  "maps/(.*)?map_id=80001"
+
+$done({path});
